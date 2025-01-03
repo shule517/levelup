3098caccc7b5f74cb546a53c9f3eafa9bb3b2785
shule517 <shule517@gmail.com>
2025-01-03 18:30:52 +0900

    不要なコードを削除

--- scene/Player/player.gd
+++ scene/Player/player.gd
@@ -4,9 +4,6 @@ extends CharacterBody2D
 const DEADZONE: float = 0.2
 const SPEED: float    = 50.0
 
-#func _input(event: InputEvent) -> void:
-	#print(event)
-
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var overlapping_bodies = []
@@ -48,26 +45,11 @@ func select_body() -> Node2D:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy") && body.is_alive():
 		overlapping_bodies.append(body)
-		#select_target()
-		#for i in overlapping_bodies:
-			#i.set_is_selected(false)	
-
-		#overlapping_bodies.sort_custom(func(a, b): return global_position.distance_to(a.global_position) < global_position.distance_to(b.global_position))
-		#var nearest_body = overlapping_bodies[0]
-
-		#var nearest_body = overlapping_bodies.min(func(body): 
-			#return global_position.distance_to(body.global_position)
-		#)
-			
-		#nearest_body.set_is_selected(true)
-		#print(overlapping_bodies)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
 		overlapping_bodies.erase(body)
-		#select_target()
 		body.set_is_selected(false)
-		#print(overlapping_bodies)
 
 var attack_target = null
 
@@ -115,79 +97,7 @@ func _process(_delta):
 		$WeaponSprite2D.visible = false
 
 	velocity = value * SPEED
-	#print("velocity: %s" % velocity)
-	#velocity.x += value.x * SPEED
-	#print(str(value))
 	move_and_slide()
-
-	#var left_stick_x = Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
-	#var left_stick_y = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
-	#print(left_stick_y)
-	
-	#if abs(left_stick_x) > DEADZONE:
-		#if left_stick_x > 0:
-			#print("Right")
-		#else:
-			#print("Left")
-	#
-	#if abs(left_stick_y) > DEADZONE:
-		#if left_stick_y > 0:
-			#print("Down")
-		#else:
-			#print("Up")
-
-#extends CharacterBody2D
-#
-#
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = 0 #ProjectSettings.get_setting("physics/2d/default_gravity")
-#
-#func _ready():
-	#Input.joy_connection_changed.connect(_on_joy_connection_changed)
-#
-#func _on_joy_connection_changed(device:int, connected:bool):
-	#if not connected:
-		#print("デバイス%sのゲームパッドが切断されました" % str(device))
-	#elif connected:
-		#print("デバイス%sのゲームパッドが接続されました" % str(device))
-##func _input(event):
-	##print(event)
-#
-##func _process(delta):
-	##var value :Vector2 = Input.get_vector("left_stick_right", "left_stick_left", "left_stick_up", "left_stick_down")
-	##print(str(value))
-#
-#func _unhandled_input(event):
-	#print(event)
-	##if event is InputEventKey:
-		##if event.pressed and event.scancode == KEY_ESCAPE:
-			##get_tree().quit()
-#
-##func _physics_process(delta):
-	##move_and_slide()
-#
-#
-##func _physics_process(delta):
-	### Add the gravity.
-	##if not is_on_floor():
-		##velocity.y += gravity * delta
-##
-	### Handle jump.
-	##if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		##velocity.y = JUMP_VELOCITY
-##
-	### Get the input direction and handle the movement/deceleration.
-	### As good practice, you should replace UI actions with custom gameplay actions.
-	##var direction = Input.get_axis("ui_left", "ui_right")
-	##if direction:
-		##velocity.x = direction * SPEED
-	##else:
-		##velocity.x = move_toward(velocity.x, 0, SPEED)
-##
-	##move_and_slide()
 
 func attack() -> void:
 	if is_instance_valid(attack_target) && attack_target.is_alive():
