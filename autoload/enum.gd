bd2a022e24719348ec82f8bbca49a014efe3a8f8
shule517 <shule517@gmail.com>
2025-01-21 23:58:48 +0900

    リファクタリング

--- autoload/enum.gd
+++ autoload/enum.gd
@@ -1,11 +1,18 @@
 extends Object
 class_name Enums
 
-enum Direction { UP, DOWN, LEFT, RIGHT }
-enum Field { SPRING, SAND, WINTER }
+enum Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
 
-static func get_field_as_string(enum_value: int) -> String:
-	for name: String in Enums.Field.keys():
-		if Enums.Field[name] == enum_value:
-			return name
-	return "Unknown"  # 未定義の値の場合
+enum Field {
+	SPRING,
+	SAND,
+	WINTER
+}
+
+static func field_to_string(enum_value: int) -> String:
+	return Enums.Field.keys()[enum_value]
--- autoload/field_switcher.gd
+++ autoload/field_switcher.gd
@@ -5,6 +5,4 @@ func switch(field: Enums.Field) -> void:
 	get_tree().change_scene_to_file(scene_path)
 
 func get_scene_path(field: Enums.Field) -> String:
-	var field_name := Enums.get_field_as_string(field)
-	var path: String = "res://Fields/field_%s.tscn" % field_name
-	return path
+	return "res://Fields/field_%s.tscn" % Enums.field_to_string(field)
