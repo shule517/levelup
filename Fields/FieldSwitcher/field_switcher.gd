2b09c9951b55690ec45765ced0ede52ab33890ec
shule517 <shule517@gmail.com>
2025-01-22 09:04:31 +0900

    不要なコードを削除した

--- Fields/FieldSwitcher/field_switcher.gd
+++
@@ -1,5 +0,0 @@
-class_name FieldSwitcher
-extends Node
-
-func switch(destination_field_scene: PackedScene, destination_door_name: String) -> void:
-	get_tree().change_scene_to_packed(destination_field_scene)
