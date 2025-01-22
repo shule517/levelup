e353b2e4c0edc7d5f1480c3c09922268c9ea899b
shule517 <shule517@gmail.com>
2025-01-22 09:15:43 +0900

    不要なコードを削除した

--- Fields/Warp/warp.gd
+++ Fields/Warp/warp.gd
@@ -8,10 +8,6 @@ extends Area2D
 @onready var spawn_maker_2d: Marker2D = $SpawnMarker2D
 @onready var label: Label = $Label
 
-#const spring_scene: PackedScene = preload("res://Fields/field_spring.tscn")
-#const sand_scene: PackedScene = preload("res://Fields/field_sand.tscn")
-#const winter_scene: PackedScene = preload("res://Fields/field_winter.tscn")
-
 func _get_enum_as_string(enum_value: int) -> String:
 	for name: String in Enums.Field.keys():
 		if Enums.Field[name] == enum_value:
@@ -24,16 +20,3 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		FieldSwitcher.switch(field)
-
-#func get_scene() -> PackedScene:
-	#return sand_scene
-
-#func get_scene() -> PackedScene:
-	#match field:
-		#Field.SPRING:
-			#return spring_scene
-		#Field.SAND:
-			#return sand_scene
-		#Field.WINTER:
-			#return winter_scene
-	#return
--- scene/UI/PlayerStatus/player_status.gd
+++ scene/UI/PlayerStatus/player_status.gd
@@ -13,13 +13,4 @@ func _process(delta: float) -> void:
 まもり： %d
 はやさ： %d
 " % [global.player_level, global.player_hp, global.player_max_hp, global.player_atk, global.player_def, 200 - 50/(1.0 / global.player_atk_speed)]
-# global.player_next_exp - global.player_exp
 	$Window/Control/ExpProgressBar.value = global.player_exp * 100 / global.player_next_exp
-
-#@export var player_max_hp: int = 28
-#@export var player_hp: int = 28
-#@export var player_atk: int = 11
-#@export var player_def: int = 3
-#@export var player_exp: int = 0
-#@export var player_next_exp: int = 1
-#@export var player_level: int = 1
