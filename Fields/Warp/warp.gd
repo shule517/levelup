0ac785d59cf07f7e18c222ab0d80fe9e6fb55eef
shule517 <shule517@gmail.com>
2025-01-22 23:33:33 +0900

    不要なコードを削除した

--- Fields/Warp/warp.gd
+++ Fields/Warp/warp.gd
@@ -8,14 +8,8 @@ extends Area2D
 @onready var spawn_maker_2d: Marker2D = $SpawnMarker2D
 @onready var label: Label = $Label
 
-func _get_enum_as_string(enum_value: int) -> String:
-	for name: String in Enum.Field.keys():
-		if Enum.Field[name] == enum_value:
-			return name
-	return "Unknown"  # 未定義の値の場合
-
 func _ready() -> void:
-	label.text = "to %s" % _get_enum_as_string(field)
+	label.text = "to %s" % Enum.field_to_string(field)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
