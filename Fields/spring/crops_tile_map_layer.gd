abe1985fe060a6cd1648c6350d384347bb78d671
shule517 <shule517@gmail.com>
2025-02-16 08:37:19 +0900

    MapLayerに作物を植えることができた

--- Fields/spring/crops_tile_map_layer.gd
+++ Fields/spring/crops_tile_map_layer.gd
@@ -1,3 +1,4 @@
+@tool
 extends TileMapLayer
 
 func _ready() -> void:
@@ -6,3 +7,11 @@ func _ready() -> void:
 
 	for i in range(20):
 		set_cell(Vector2i(-i, -i), 0)
+
+	# TODO: MapLayerの座標の取得
+	var pos: Vector2i = local_to_map(Vector2(0, 0))
+	print(pos)
+	
+	# TODO: tunipを植える
+	for i in range(20):
+		set_cell(Vector2i(i, i), 0, Vector2i(0, 0), 1)
--- crops/turnip.gd
+++ crops/turnip.gd
@@ -6,15 +6,20 @@ extends Node2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
-	sprite.play("2")
+	sprite.frame = 0
 	audio_stream_player_2d.finished.connect(_on_finished)
 
 func _on_finished() -> void:
 	queue_free()
 
+# 収穫可能か？
+func is_harvestable() -> bool:
+	# 最大フレームまでいったか？
+	return sprite.frame == (sprite.sprite_frames.get_frame_count(sprite.animation) - 1)
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		if Input.is_action_pressed("button_a"):
+		if 	is_harvestable() and Input.is_action_pressed("button_a"):
 			visible = false
 			play_sound_effect(sound, randf_range(0.8, 1.5))
 
@@ -25,3 +30,6 @@ func play_sound_effect(sound_effect: AudioStream, pitch_scale: float = 1.0, volu
 	audio_stream_player_2d.volume_db = volume_db
 	audio_stream_player_2d.play()
 	await audio_stream_player_2d.finished
+
+func _on_timer_timeout() -> void:
+	sprite.frame += 1
