e6739185289c2eeda89a5021e898f4c8b58a0f7d
shule517 <shule517@gmail.com>
2025-02-20 00:39:14 +0900

    収穫できる時にアウトラインを表示して ガイドを表示する

--- crops/turnip.gd
+++ crops/turnip.gd
@@ -2,16 +2,29 @@
 class_name Turnip
 extends Node2D
 
+# TODO: Cropを継承したい
+
 @export var sound: AudioStream
 @onready var seed_sprite: AnimatedSprite2D = $SeedAnimatedSprite2D
 @onready var crop_sprite: AnimatedSprite2D = $CropAnimatedSprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var player: Player = get_tree().get_nodes_in_group("Player")[0]
 
+var is_selected: bool = false
+
 func _ready() -> void:
 	seed_sprite.visible = true
 	seed_sprite.frame = randi_range(0, 3) # 種4種類ランダム
 	crop_sprite.visible = false
+
+	var new_material := ShaderMaterial.new()
+	new_material.shader = crop_sprite.material.shader
+	crop_sprite.material = new_material
+
+func _process(delta: float) -> void:
+	if is_selected && can_harvest():
+		if Input.is_action_pressed("button_a"):
+			harvest()
 
 # 収穫可能か？
 func can_harvest() -> bool:
@@ -25,9 +38,17 @@ func harvest() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	# TODO: 収穫処理はここで書くのがいいのか？ Playerからキックしたほうがいい？
-	if body.is_in_group("Player") and Input.is_action_pressed("button_a"):
+	if body.is_in_group("Player"):
+		is_selected = true
 		if can_harvest():
-			harvest()
+			crop_sprite.material.set_shader_parameter("enabled", true)
+			crop_sprite.material.set_shader_parameter("is_selected", true)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_selected = false
+		crop_sprite.material.set_shader_parameter("enabled", false)
+		crop_sprite.material.set_shader_parameter("is_selected", false)
 
 # 植物の成長
 func _on_timer_timeout() -> void:
