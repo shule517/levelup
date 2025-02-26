25ad4c75675cde9cbcfcccb4a9b3b615a091fde3
shule517 <shule517@gmail.com>
2025-02-26 22:34:17 +0900

    耕すアニメーションを追加した

--- scene/Player/player.gd
+++ scene/Player/player.gd
@@ -1,12 +1,15 @@
 class_name Player
 extends CharacterBody2D
 
+@export var till_sound: AudioStream
+
 @onready var player_battle: Node = $PlayerBattle
 @onready var ray_cast: RayCast2D = $RayCast2D
 @onready var hal_sprite: AnimatedSprite2D = $HalAnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CellArea2D/CollisionShape2D
 @onready var crops_tile_map_layer: CropsTileMapPlayer = get_tree().get_root().find_child("CropsTileMapLayer", true, false)
 @onready var ground_tile_map_layer: GroundTileMapPlayer = get_tree().get_root().find_child("GroundTileMapLayer", true, false)
+@onready var cell_animated_sprite_2d: AnimatedSprite2D = $CellAnimatedSprite2D
 
 const SPEED: float    = 50.0
 
@@ -17,15 +20,23 @@ func _process(delta: float) -> void:
 	if is_watering:
 		return  # 水やり中はフリーズ
 
+	var ray_shape := collision_shape.shape as SeparationRayShape2D
+	var local_tip_position := Vector2(position.x + ray_shape.length, position.y)
+
+	# TODO: 該当セルの状態を確認する。耕されているか
+
 	# 種をまく
 	if Input.is_action_pressed("button_a") and crops_tile_map_layer:
-		# 耕す
-		ground_tile_map_layer.till_soil(Vector2(position.x, position.y + 16))
-		# 種を植える
-		crops_tile_map_layer.plant_crop(Vector2(position.x, position.y + 16))
+		ground_tile_map_layer.till_soil(Vector2(local_tip_position.x, local_tip_position.y)) # 耕す
+		crops_tile_map_layer.plant_crop(Vector2(local_tip_position.x, local_tip_position.y)) # 種を植える
+		is_watering = true # 操作をフリーズ
+		hal_sprite.play("soil")
+		Audio.play_sound_effect(till_sound, self, randf_range(0.8, 1.5))
+		await get_tree().create_timer(0.5).timeout
+		is_watering = false # 操作を再開
 
 	# TODO: 種をまくセルのガイド表示
-	#cell_animated_sprite_2d.global_position = Vector2(int(owner.global_position.x / 16) * 16 + 8, int(owner.global_position.y / 16) * 16 + 8)
+	cell_animated_sprite_2d.global_position = Vector2(int(local_tip_position.x / 16) * 16 + 8, int(local_tip_position.y / 16) * 16 + 8)
 
 	# 水やり、収穫
 	if crops:
