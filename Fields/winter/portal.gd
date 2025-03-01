a885331fbc7933d6f16ce60b08df217e828b788e
shule517 <shule517@gmail.com>
2025-03-02 06:18:49 +0900

    ポータルの追加

---
+++ Fields/winter/portal.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var shader_material: ShaderMaterial = ShaderMaterial.new()
+
+func _process(delta: float) -> void:
+	animated_sprite.play("default")
+
+	shader_material.shader = animated_sprite.material.shader
+	animated_sprite.material = shader_material
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		shader_material.set_shader_parameter("enabled", true)
+		shader_material.set_shader_parameter("is_selected", true)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		shader_material.set_shader_parameter("is_selected", false)
