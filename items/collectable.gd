fb3fd2f7b724034373b7cd2836bb2bfaedc11488
shule517 <shule517@gmail.com>
2025-03-05 07:29:31 +0900

    アイテムゲットした時の音を追加

--- items/collectable.gd
+++ items/collectable.gd
@@ -2,7 +2,9 @@ class_name Collectable
 extends StaticBody2D
 
 @export var item_resource: Item
+@export var collect_sound: AudioStream
 
 func collect(inventory: Inventory) -> void:
 	inventory.insert(item_resource)
+	Audio.play_sound_effect(collect_sound, self, randf_range(0.8, 1.5))
 	queue_free()
